a7b7baf95dc31bc52d9c718371d931dd9797b073
boloraul1 <boloraul1@gmail.com>
2022-11-06 18:32:02 +0100

    Cuarto commit

--- canviDbase/src/oovv/NumB10.java
+++ canviDbase/src/oovv/NumB10.java
@@ -9,14 +9,37 @@ package oovv;
  * @author bolor
  */
 public class NumB10 {
+    /** 
+*	constructor de NumB10. número en base 10 
+*	@param num valor del número en base 10 
+*/ 
+
     public NumB10(int num) { 
         this.num = num; 
     } 
- 
+ /** 
+*	assigna el valor al número. 
+*	@param num valor a assignar 
+ */ 
+
     public void setNum(int num) {         this.num = num; 
     } 
  
-    @Override     public String toString() {         return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    @Override 
+public String toString() { 
+return num; // (error 1)
+} 
+    public String canviaABase(int base) {     if(base<2||base>24){ 
+        throw new ArithmeticException("la base va de 2 a 24"); 
     } 
+String digits = "0123456789ABCDEFGHIJKLMN; // (error 2)
+    String cad = ""; 
+while (num > “O”) { // (error 3)
+        cad = cad + digits.charAt(num % base);         num = num / base; 
+    } 
+    return alReves(cad);  // crida al mètode alReves 
+} 
+
+    
 
 }

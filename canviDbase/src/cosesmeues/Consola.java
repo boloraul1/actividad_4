27cac36efacab46dfbed22e185075b96d8727b28
boloraul1 <boloraul1@gmail.com>
2022-11-06 18:16:46 +0100

    Segundo commit

--- canviDbase/src/cosesmeues/Consola.java
+++ canviDbase/src/cosesmeues/Consola.java
@@ -14,11 +14,37 @@ import java.io.InputStreamReader;
  */
 public class Consola {
 
+    /**
+     * llig una cadena de text de teclat.
+     *
+     * @return la cadena llegida
+     * @throws IOException
+     */
     public static String lligText() throws IOException {
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
         return lector.readLine();
     }
 
- 	// es repeteix mentre la transformació falla 
+    /**
+     * llig un enter de teclat. el procés de lectura es repeteix mentre el valor
+     * no siga un enter
+     *
+     * @return el número llegit
+     * @throws IOException
+     */
+
+    public static int lligEnter() throws IOException {
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        boolean incorrecte = true;
+        do {
+            try {
+                int valor = Integer.parseInt(lector.readLine());  	// transforma el text a enter 
+                return valor;  	// retorna l'enter resultant de la transformació 
+            } catch (NumberFormatException ex) {  	// la transformació falla i s'agafa l'excepció 
+            }
+        } while (incorrecte);
+        return -1;
+    }
+    // es repeteix mentre la transformació falla 
 
 }
